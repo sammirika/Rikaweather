62baffbc578d058a0e1ceda57d57ec83ab26a2e6
sammirika <sammirika@gmail.com>
2017-04-22 22:01:10 +0800

    用litepal创建了数据库和表的各项配置.

---
+++ app/src/main/java/com/example/sammirika/rikaweather/db/Province.java
@@ -0,0 +1,31 @@
+package com.example.sammirika.rikaweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Sammirika on 2017/4/22.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void  setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
